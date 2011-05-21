1de0b41442357f3a9c2f21a98a311afd156d2374
Sam John & Sean Moon <pair+samantha+sean@kangaroost.com>
2011-05-21 18:46:56 -0400

    google maps view, Smoon: fuck it!

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.7'
 gem 'heroku'
 gem 'instagram'
 gem 'haml'
+gem "jquery-rails"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $("#map_canvas").width("500px");
+  $("#map_canvas").height("500px");
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    var latlng = new google.maps.LatLng(lat,lng);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map($("#map_canvas").get(0), myOptions);
+  });
+});
