7e291cb2292d0ec07aba9e7bb7cbea2541e699f0
Sam John & Sean Moon <pair+samantha+sean@kangaroost.com>
2011-05-22 09:22:01 -0700

    adding google analytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,14 @@
 $(function() {
   $("#map_canvas").hide();
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23507228-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 
   navigator.geolocation.getCurrentPosition(function(position) {
 
