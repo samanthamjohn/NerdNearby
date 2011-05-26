98a8db71e70731ed1d58c95e56364e91d015c36f
Evan Farrar & Sean Moon <pair+evan+sean@nerdnearby.com>
2011-05-25 22:44:03 -0400

    adding back in foursquare

--- app/controllers/feed_items_controller.rb
+++ app/controllers/feed_items_controller.rb
@@ -30,27 +30,23 @@ class FeedItemsController < ApplicationController
 
 
   def call_foursquare
-    # foursquare = Foursquare::Base.new(ENV["FOURSQUARE_CLIENT_ID"], ENV["FOURSQUARE_CLIENT_SECRET"])
-    # foursquare_venues = []
-    # foursquare.venues.nearby(ll: "#{params[:lat]}, #{params[:lng]}").map do |venue|
-      # distance = venue.json["location"]["distance"]
-      # fs_venue = foursquare.venues.find(venue.json["id"])
-      # fs_venue.json["tips"]["groups"].first["items"].each do |tip|
-        # time = Time.at(tip["createdAt"].to_i)
-        # if time > Time.now - 12.years
-          # foursquare_venues.push(
-          # {
-            # venue: venue.json["name"],
-            # distance: distance,
-            # time: Time.at(tip["createdAt"].to_i),
-            # text: tip["text"],
-            # feed_item_type: "foursquare"
-          # }
-          # )
-        # end
-      # end
-    # end
+    foursquare = Foursquare::Base.new(ENV["FOURSQUARE_CLIENT_ID"], ENV["FOURSQUARE_CLIENT_SECRET"])
     foursquare_venues = []
+    foursquare.venues.nearby(ll: "#{params[:lat]}, #{params[:lng]}").map do |venue|
+      distance = venue.json["location"]["distance"]
+      foursquare_venues.push({
+        venue: venue.json["name"],
+        distance: distance,
+        text: "#{venue.stats["checkinsCount"]} check-ins",
+        time: Time.now - (rand(60)).minutes,
+        url: "http://foursquare.com",
+        feed_item_type: "foursquare",
+        checkins: venue.stats["checkinsCount"],
+        address: venue.location["address"]
+      })
+    end
+    foursquare_venues ||= []
+    foursquare_venues.sort{|a,b| b["checkins"] <=> a["checkins"]}[0..10]
   end
 
   def call_instagram
@@ -76,7 +72,6 @@ class FeedItemsController < ApplicationController
     flickr_pictures = flickr.photos.search(args).collect do |flickr_photo|
       # info = flickr.photos.getInfo({photo_id: flickr_photo.id, secret: flickr_photo.secret})
       # time: Time.parse(info.dates.try(:taken))
-      # this kinda shit gives our thing character ^
       {
         image_tag: FlickRaw.url(flickr_photo),
         feed_item_type: "flickr",
